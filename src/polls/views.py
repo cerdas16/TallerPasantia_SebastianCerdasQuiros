57ed1411673c74dcff39c3f35bb997b5dbed4b4e
Sebastian Cerdas <sebastian.cerdas.quiros.lte38@gmail.com>
2024-08-12 15:52:47 +0200

    parte 3 del tutorial completo
    Signed-off-by: Sebastian Cerdas <sebastian.cerdas.quiros.lte38@gmail.com>

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -7,12 +8,23 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(request, "polls/index.html", context)
 
+
+# def detail(request, question_id):
+
+#    try:
+#      question = Question.objects.get(pk=question_id)
+#    except Question.DoesNotEXist:
+#        raise Http404("Question does not exist")
+#   return render(request, "polls/details.html", {"question": question})
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/details.html", {"question": question})
 
 
 def results(request, question_id):
